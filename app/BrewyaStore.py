06d8b178985ec2896fe070d6bef03867ba3063de
Jiri 'Ghormoon' Novak <jiri.novak@ghorland.net>
2019-06-21 20:54:46 +0200

    static files WIP

--- app/BrewyaStore.py
+++ app/BrewyaStore.py
@@ -1,11 +1,22 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, send_from_directory, Response
+import magic
 
 app = Flask(__name__)
 
+self_url = 'http://10.42.42.233:5000'
+static_data = {}
+
+def replace_vars(data):
+    data = data.replace("{this_server_url}", self_url)
+    return data
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return '', 204
+
+@app.route("/api/v1/status")
+def status():
+    return '', 204
 
 @app.route("/api/v1/console_configuration")
 def console_configuration():
@@ -19,15 +30,55 @@ def ratings():
     "ratings": []
 }'''
 
+@app.route("/api/v1/details")
+def details():
+    tmp = None
+    with open("app/data/games/{}/detail.json".format(request.args.get('app'))) as f:
+        tmp = "\n".join(f.readlines())
+    return tmp
+
+@app.route("/api/v1/apps/<uuid>/download")
+def apps_download(uuid):
+    tmp = None
+    with open("app/data/games/{}/download.json".format(uuid)) as f:
+        tmp = "\n".join(f.readlines())
+    return tmp
+
+@app.route("/api/v1/apps/<uuid>/<resource>")
+def apps_resource(uuid, resource):
+    filename = "app/data/games/{}/static/{}".format(uuid, resource)
+    with open(filename,'rb') as f:
+        return Response(b"".join(f.readlines()), magic.from_file(filename, mime=True))
+
 @app.route("/api/v1/sessions", methods=['POST'])
 def sessions():
     return '''{
     "token": "7e9d69a1-311d-4add-b410-faf969ed499c"
 }'''
 
-@app.route("/api/v1/gamers/me/consoles", methods=['GET', 'POST'])
-def consoles():
+@app.route("/api/v1/gamers/me/consoles", methods=['GET', 'POST', 'PUT'])
+def gamers_consoles():
     return '{}'
+
+@app.route("/api/v1/gamers/key", methods=['PUT', 'POST'])
+def gamers_key():
+    return '{}', 201
+
+@app.route("/api/v1/premium_purchases", methods=['GET'])
+def premium_purchases():
+    return '{ "games": [] }'
+
+@app.route("/api/v1/gamers/me", methods=['GET'])
+def gamers_me():
+    return '''{
+  "gamer": {
+    "uuid": "f791f35c-697c-4d4b-8298-783e70622ef2",
+    "settings": {},
+    "founder": false,
+    "email": "nobody@brewya.tv",
+    "username": "BrewyaUser"
+  }
+}'''
 
 @app.route("/api/v1/wallet")
 def wallet():
@@ -65,33 +116,11 @@ def discover_home():
 
 @app.route("/api/v1/discover")
 def discover():
-    return '''{
-        "heroRow": null, 
-        "rows": [
-            {
-                "ranked": true, 
-                "showPrice": true, 
-                "tiles": [], 
-                "title": "OUR NEW STORE"
-            }, 
-            {
-                "ranked": false, 
-                "showPrice": true, 
-                "tiles": [], 
-                "title": "JUST A DEMO"
-            }, 
-            {
-                "ranked": false, 
-                "showPrice": true, 
-                "tiles": [], 
-                "title": "NOTHING HERE"
-            } 
-        ], 
-        "tiles": [
-        ], 
-        "title": "DISCOVER"
-    }'''
+    return static_data['discover']
 
 
 if __name__ == "__main__":
+    with open("app/data/discover.json","r") as f:
+        static_data['discover'] = "\n".join(f.readlines())
+        static_data['discover'] = replace_vars(static_data['discover'])
     app.run(host='0.0.0.0')
